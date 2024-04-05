3ec5d3f6656a70705e77d26c6f6d8c3d29fb9161
lorinspan <lorinspanx@gmail.com>
2024-04-06 01:12:08 +0300

    commit

--- src/app/navigation/nav.component.ts
+++ src/app/navigation/nav.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
-
+  navigateTo(href: string): void {
+    switch (href) {
+      case 'instagram':
+        href = 'https://www.instagram.com/lorinspan/';
+        break;
+      case 'flickr':
+        href = 'https://www.flickr.com/photos/200472052@N08/';
+        break;
+      case 'linkedin':
+        href = 'https://www.linkedin.com/in/lorin-%C8%99pan-831994214/';
+        break;
+      case 'mail':
+        href = 'mailto:lorinspanx@gmail.com';
+        break;
+      default:
+        return;
+    }
+    window.location.href = href;
+  }
 }
