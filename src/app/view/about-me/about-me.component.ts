c3a7c52d34eb70b19dab7ff8067f3416e6aab7f0
lorinspan <lorinspanx@gmail.com>
2024-04-08 18:02:04 +0300

    commit

--- src/app/view/about-me/about-me.component.ts
+++ src/app/view/about-me/about-me.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent {
+  constructor() {}
 
+  navigateToMail() {
+    window.location.href = 'mailto:lorinspanx@gmail.com';
+  }
+
+  downloadCV() {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '/assets/CV/Lorin-Span-Resume.pdf'); // Update the file path as needed
+    link.setAttribute('download', 'lorin-span-resume.pdf'); // Update the file name as needed
+    document.body.appendChild(link);
+    link.click();
+    link.remove(); // Clean up
+  }
 }
